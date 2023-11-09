0b7559e156d52e9de2546edc460fafb50ab4a7a6
Mhinoverse <Mhinolv@gmail.com>
2023-11-08 17:15:01 -0800

    Added REVIEWS into Raw Data. Tested more test-cases in LLM with Pandas. Updated LLM_est

--- Scripts/llm_test.py
+++ Scripts/llm_test.py
@@ -1,5 +1,13 @@
 import llm
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+dataPath = '/Users/MarkHinojosa/llm-base/Raw Data/REVIEWS.csv'
+reviewDF = pd.read_csv(dataPath)
+sampleText = reviewDF['text_'].head(20)
+
 model = llm.get_model("wizardlm-13b-v1")
-response = model.prompt("Can you create a 5-day learning plan for the Pandas library in Python",
-                        system="Asnwer like a Computer Science professor")
+response = model.prompt(f"Give me a sumary these reviews : {sampleText}",
+                        system="Asnwer like a Data Analyst")
 print(response.text())
